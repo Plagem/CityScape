f1b9f77032661c2b63149ae6a3fd5cc0a920687c
jinwoo jung <alamash@naver.com>
2023-10-31 18:30:51 +0900

    음량ui생성

--- CityScape/Assets/Scripts/GameManager.cs
+++ CityScape/Assets/Scripts/GameManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Accessibility;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance = null; //싱글톤 디자인 // 외부에서 GameManager에 접근할때 이걸 끌어다가 접근
+    [SerializeField] private GameObject settingPanel;
+    [SerializeField] private GameObject audioPanel;
+
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void ShowSettingPanel()
+    {
+        settingPanel.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowAudioPanel()
     {
-        
+        audioPanel.SetActive(true);
     }
 }
