c67641f77410f370c66cb78b55800b252cb40fc8
jinwoo jung <alamash@naver.com>
2023-11-01 20:23:39 +0900

    [feat]크레딧화면 구현
    설정창 버튼을 구현, 설정창 안에 있는 음량 조절, 크레딧, 게임 종료 버튼 구현

--- CityScape/Assets/Scripts/GameManager.cs
+++ CityScape/Assets/Scripts/GameManager.cs
@@ -7,7 +7,8 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null; //싱글톤 디자인 // 외부에서 GameManager에 접근할때 이걸 끌어다가 접근
-
+    [SerializeField] private GameObject settingSound;
+    [SerializeField] private GameObject creditPanel;
     void Awake()
     {
         if (instance == null)
@@ -19,5 +20,30 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    public void OnSettingSoundPanel()
+    {
+        settingSound.SetActive(true);
+    }
+
+    public void OffSettingSoundPanel()
+    {
+        settingSound.SetActive(false);
+    }
+
+    public void OnCreditPanel()
+    {
+        creditPanel.SetActive(true);
+    }
+
+    public void OffCreditPanel()
+    {
+        creditPanel.SetActive(false);
+    }
+    
+    public void OnExitButton()
+    {
+        Application.Quit();
     }
 }
