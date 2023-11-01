b539e400506cbb92a300d72859c66f282c9ee2c9
jinwoo jung <alamash@naver.com>
2023-11-01 18:44:53 +0900

    [feat]싱글톤 디자인 추가
    게임매니저 스크립트에 싱글톤 디자인 추가

--- CityScape/Assets/Scripts/GameManager.cs
+++ CityScape/Assets/Scripts/GameManager.cs
@@ -7,8 +7,6 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null; //싱글톤 디자인 // 외부에서 GameManager에 접근할때 이걸 끌어다가 접근
-    [SerializeField] private GameObject settingPanel;
-    [SerializeField] private GameObject audioPanel;
 
     void Awake()
     {
@@ -21,14 +19,5 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-    }
-    public void ShowSettingPanel()
-    {
-        settingPanel.SetActive(true);
-    }
-
-    public void ShowAudioPanel()
-    {
-        audioPanel.SetActive(true);
     }
 }
